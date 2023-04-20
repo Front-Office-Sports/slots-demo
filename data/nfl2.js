3cb3c1a8218e6dfb931ffc06059b5a9831bfce90
Vlad Verba <vv2298@columbia.edu>
2023-04-20 16:49:19 -0400

    new design and data

---
+++ data/nfl2.js
@@ -0,0 +1,109 @@
+const dataList = [
+  // Cities
+  [
+    {
+      text: "San Francisco",
+      image:
+        "https://pbs.twimg.com/profile_images/1619835074832121857/B5nyR3ke_400x400.jpg",
+    },
+    {
+      text: "Chicago",
+      image:
+        "https://cdn.bleacherreport.net/images_root/slides/photos/000/605/306/Chicago_Bears_original.jpg?1293864336",
+    },
+    {
+      text: "New York",
+      image:
+        "https://yt3.googleusercontent.com/POBEBuwrnjqmR0v_ooItFlXZvnxXaNk7zKddZUj2hMQeea_A0RX5GsYO3P7aRZpla3L2nMGH9A=s900-c-k-c0x00ffffff-no-rj",
+    },
+    {
+      text: "Cincinnati",
+      image:
+        "https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/116254128_10158460529483416_7312040182353836380_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cZ1kme6xoYEAX8GClNO&_nc_ht=scontent-lga3-2.xx&oh=00_AfCbKacChpkZbb3d-Bi0rlZYNmxt5xbLHZfsVpWGPxv61A&oe=6469184C",
+    },
+    {
+      text: "Buffalo",
+      image:
+        "https://lh3.googleusercontent.com/jRtcRZpf8CjX8K6kYgUufqNpxB2w3f81C8W0DMZLREjF6J5NMzuNI5dtnbWaxYrNPUkCumNcKYw=w1440-ns-nd-rj",
+    },
+    {
+      text: "Denver",
+      image:
+        "https://yt3.googleusercontent.com/ytc/AGIKgqOPPudXSkXoYtxf9oYRcF5q_0kyqwoSnZRGKDFgTw=s900-c-k-c0x00ffffff-no-rj",
+    },
+    {
+      text: "Cleveland",
+      image:
+        "https://pbs.twimg.com/profile_images/1605638768811249681/LjDZjqq5_400x400.jpg",
+    },
+    {
+      text: "Dallas",
+      image:
+        "https://play-lh.googleusercontent.com/eW51OTY4lMsH2l6nyZzog7V8w7l55xUEJIAjFzMYhJgc0qFQkyhAgolZdcR8Kyx9OA=w240-h480-rw",
+    },
+    {
+      text: "Miami",
+      image:
+        "https://cdn.dribbble.com/users/894798/screenshots/14419361/media/3ad79f41215a4e4d859889f446987c6a.jpg?compress=1&resize=400x300",
+    },
+    {
+      text: "Philadelphia",
+      image:
+        "https://www.the-sun.com/wp-content/uploads/sites/6/2022/04/eagles-logo.png?strip=all&quality=100&w=1080&h=1080&crop=1",
+    },
+  ],
+
+  //Quarterbacks
+  [
+    {
+      text: "Patrick Mahomes",
+      image:
+        "https://kake.images.worldnow.com/images/23943049_G.jpeg?auto=webp&disable=upscale&height=560&fit=bounds&lastEditedDate=1673387390000",
+    },
+    {
+      text: "Justin Herbert",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Justin_Herbert_2021.jpg/1200px-Justin_Herbert_2021.jpg",
+    },
+    {
+      text: "Tom Brady",
+      image:
+        "https://www.gannett-cdn.com/presto/2021/06/18/USAT/a1a73c8b-2f4e-41d1-a32b-d3c57bebbb14-USATSI_15549316.jpg?crop=4350,2447,x0,y349&width=3200&height=1801&format=pjpg&auto=webp",
+    },
+    {
+      text: "Kirk Cousins",
+      image:
+        "https://sportsnaut.com/wp-content/uploads/2023/04/USATSI_19797623-scaled.jpg",
+    },
+    {
+      text: "Joe Burrow",
+      image:
+        "https://www.gannett-cdn.com/presto/2023/04/18/USAT/105998ec-05f2-4928-932f-99329388c0b1-GTY_1366329946.jpg",
+    },
+    {
+      text: "Jared Goff",
+      image:
+        "https://www.gannett-cdn.com/presto/2022/12/24/PDTF/00504f33-0ea6-4eda-aa23-2476a1b437e5-GTY_1452231014.jpg",
+    },
+    {
+      text: "Dak Prescott",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/ZOOuWlUAJwcln_gZgIwvd48Y1UQ=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24578468/1458617309.jpg",
+    },
+    {
+      text: "Jalen Hurts",
+      image:
+        "https://media.cnn.com/api/v1/images/stellar/prod/230417122441-01-jalen-hurts-file.jpg?c=original",
+    },
+    {
+      text: "Aaron Rodgers",
+      image:
+        "https://static.clubs.nfl.com/image/private/t_person_squared_mobile/f_auto/packers/dlzoytukwspoctz2scc4.jpg",
+    },
+    {
+      text: "Josh Allen",
+      image:
+        "https://static.foxnews.com/foxnews.com/content/uploads/2023/01/Josh-Allen8.jpg",
+    },
+  ],
+];
