b6b6562b71df3259610d946a1ae0663d452e4876
Vlad Verba <vv2298@columbia.edu>
2023-07-27 16:53:57 -0400

    new quarterbacks data

---
+++ data/nfl3.js
@@ -0,0 +1,81 @@
+const dataList = [
+  // Cities
+  [],
+  // Team Owners
+  [],
+  // Head Coaches
+  [],
+  // Quarterbacks
+  [
+    { text: "Lamar Jackson", image: "images/quarterbacks/Lamar Jackson.jpg" },
+    { text: "Josh Allen", image: "images/quarterbacks/Josh Allen.jpeg" },
+    { text: "Joe Burrow", image: "images/quarterbacks/Joe Burrow.jpg" },
+    { text: "Deshaun Watson", image: "images/quarterbacks/Deshaun Watson.jpg" },
+    {
+      text: "Russell Wilson",
+      image: "images/quarterbacks/Russell_Wilson.jpeg",
+    },
+    { text: "C.J. Stroud", image: "images/quarterbacks/C.J. Stroud.jpg" },
+    {
+      text: "Anthony Richardson",
+      image: "images/quarterbacks/Anthony Richardson.jpeg",
+    },
+    {
+      text: "Gardner Minshew",
+      image: "images/quarterbacks/Gardner Minshew.jpeg",
+    },
+    {
+      text: "Trevor Lawrence",
+      image: "images/quarterbacks/Trevor Lawrence.jpeg",
+    },
+    {
+      text: "Patrick Mahomes",
+      image: "images/quarterbacks/Patrick Mahomes.jpeg",
+    },
+    {
+      text: "Jimmy Garoppolo",
+      image: "images/quarterbacks/Jimmy Garoppolo.jpeg",
+    },
+    {
+      text: "Justin Herbert",
+      image: "images/quarterbacks/Justin Herbert.jpeg",
+    },
+    {
+      text: "Tua Tagovailoa",
+      image: "images/quarterbacks/Tua Tagovailoa.jpeg",
+    },
+    { text: "Mac Jones", image: "images/quarterbacks/Mac Jones.jpg" },
+    { text: "Aaron Rodgers", image: "images/quarterbacks/Aaron Rodgers.jpeg" },
+    { text: "Kenny Pickett", image: "images/quarterbacks/Kenny Pickett.jpeg" },
+    {
+      text: "Ryan Tannehill",
+      image: "images/quarterbacks/Ryan Tannehill.jpeg",
+    },
+    { text: "Will Levis", image: "images/quarterbacks/Will Levis.jpeg" },
+    { text: "Desmond Ridder", image: "images/quarterbacks/Desmond Ridder.jpg" },
+    { text: "Kyler Murray", image: "images/quarterbacks/Kyler Murray.png" },
+    { text: "Bryce Young", image: "images/quarterbacks/Bryce Young.jpeg" },
+    { text: "Justin Fields", image: "images/quarterbacks/Justin Fields.jpeg" },
+    { text: "Dak Prescott", image: "images/quarterbacks/Dak Prescott.jpeg" },
+    { text: "Jared Goff", image: "images/quarterbacks/Jared Goff.jpeg" },
+    { text: "Jordan Love", image: "images/quarterbacks/Jordan Lov.jpeg" },
+    {
+      text: "Matthew Stafford",
+      image: "images/quarterbacks/Matthew Stafford.jpg",
+    },
+    { text: "Kirk Cousins", image: "images/quarterbacks/Kirk Cousins.jpg" },
+    { text: "Derek Carr", image: "images/quarterbacks/Derek Carr.jpg" },
+    { text: "Daniel Jones", image: "images/quarterbacks/Daniel Jones.jpg" },
+    { text: "Jalen Hurts", image: "images/quarterbacks/Jalen Hurts.jpeg" },
+    { text: "Brock Purdy", image: "images/quarterbacks/Brock Purdy.jpeg" },
+    { text: "Trey Lance", image: "images/quarterbacks/Trey Lance.jpg" },
+    { text: "Geno Smith", image: "images/quarterbacks/Geno Smith.jpeg" },
+    { text: "Kyle Trask", image: "images/quarterbacks/Kyle Trask.jpg" },
+    { text: "Baker Mayfield", image: "images/quarterbacks/Baker Mayfield.jpg" },
+    { text: "Sam Howell", image: "images/quarterbacks/Sam Howell.jpeg" },
+    {
+      text: "Jacoby Brissett",
+      image: "images/quarterbacks/Jacoby Brissett.jpeg",
+    },
+  ],
+];
