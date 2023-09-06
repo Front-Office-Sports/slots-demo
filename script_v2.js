3358558417ba2098048a5288788acccae37e8872
Vlad Verba <vv2298@columbia.edu>
2023-09-06 13:00:09 -0400

    updated cherry image to compressed PNG

--- script_v2.js
+++ script_v2.js
@@ -1,5 +1,5 @@
 // --- Declarations --- //
-const slotsCoverImg = "images/slot_img/cherry_cover.png";
+const slotsCoverImg = "images/slot_img/cherry_cover_comp.png";
 
 // define the "doors" element
 const allDoors = document.querySelectorAll(".single-door");
